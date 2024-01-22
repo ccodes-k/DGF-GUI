2450149c0896dbe07fa92217c2e19b06e09312e1
Lan42 <junhanglan@gmail.com>
2024-01-22 15:25:42 +0800

    Add hide and show functions for View Window Add close config and view window to close event

--- src/View_GUI.py
+++ src/View_GUI.py
@@ -33,7 +33,7 @@ class ViewWindow(QWidget):
         self.b4.move(30, 130)
         self.b4.setChecked(True)
 
-        self.b5 = QtWidgets.QCheckBox("Direction",self)
+        self.b5 = QtWidgets.QCheckBox("Direction and Compass",self)
         self.b5.move(30, 170)
         self.b5.setChecked(True)
 
@@ -43,3 +43,53 @@ class ViewWindow(QWidget):
 
         self.setWindowFlag(QtCore.Qt.WindowMinimizeButtonHint, False)
         self.setWindowFlag(QtCore.Qt.WindowMaximizeButtonHint, False)
+    
+    def update_view(self,view):
+
+    # For Data Graphs
+        
+        # For Heart Rate & SpO2
+        if view.b1.isChecked():
+            self.DGW.W1.show()
+            self.DGW.W2.show()
+        elif not view.b1.isChecked():
+            self.DGW.W1.hide()
+            self.DGW.W2.hide()
+
+        # For Temperature
+        if view.b2.isChecked():
+            self.DGW.W3.show()
+        elif not view.b2.isChecked():
+            self.DGW.W3.hide()
+
+        # For Depth
+        if view.b3.isChecked():
+            self.DGW.W4.show()
+        elif not view.b3.isChecked():
+            self.DGW.W4.hide()
+
+        # For the whole Data Graphs
+        if view.b1.isChecked() or view.b2.isChecked() or view.b3.isChecked():
+            self.DGW.show()
+        elif not view.b1.isChecked() and not view.b2.isChecked() and not view.b3.isChecked():
+            self.DGW.hide()
+    
+    # For GPS
+        
+        # For Lat and Long
+        if view.b4.isChecked():
+            self.LLL.show()
+        elif not view.b4.isChecked():
+            self.LLL.hide()
+        
+        # For Compass and Direction Label
+        if view.b5.isChecked():
+            self.Compass.show()
+        elif not view.b5.isChecked():
+            self.Compass.hide()
+    
+    # For Camera
+        if view.b6.isChecked():
+            self.Cam.show()
+        elif not view.b6.isChecked():
+            self.Cam.hide()
